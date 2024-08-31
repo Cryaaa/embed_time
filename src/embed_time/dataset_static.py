dac03c59869bcfa72882b4c177e5355469007651
Matteo Di Bernardo <md3498@columbia.edu>
2024-08-31 19:43:14 +0000

    cleaned datasets (temp)

--- src/embed_time/dataset_static.py
+++ src/embed_time/dataset_static.py
@@ -8,22 +8,18 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 class ZarrCellDataset(Dataset):
-    def __init__(self, parent_dir, csv_file, split = "train", channels=[0, 1, 2, 3], transform="masks", crop_size=None):
+    def __init__(self, parent_dir, csv_file, split="train", channels=[0, 1, 2, 3], mask="masks", normalizations=None, interpolations=None):
         self.parent_dir = Path(parent_dir)
         self.channels = channels
-        self.transform = transform
-        self.crop_size = crop_size
+        self.mask = mask
+        self.normalizations = normalizations
+        self.interpolations = interpolations
 
         # Load the CSV file
         self.data_info = pd.read_csv(csv_file)
 
         # Split the data into train, val, and test
-        if split == "train":
-            self.data_info = self.data_info[self.data_info['split'] == "train"]
-        elif split == "val":
-            self.data_info = self.data_info[self.data_info['split'] == "val"]
-        elif split == "test":
-            self.data_info = self.data_info[self.data_info['split'] == "test"]
+        self.data_info = self.data_info[self.data_info['split'] == split]
 
         # Group the data by gene, barcode, and stage
         self.grouped_data = self.data_info.groupby(['gene', 'barcode', 'stage'])
@@ -50,8 +46,11 @@ class ZarrCellDataset(Dataset):
         cell_mask = zarr_group['cells'][cell_idx]
         nuclei_mask = zarr_group['nuclei'][cell_idx]
 
-        # Apply transform
-        cell_image, nuclei_image = self._apply_transform(original_image, cell_mask, nuclei_mask)
+        # Apply mask and normalization
+        cell_image, nuclei_image = self._apply_mask_normalization(original_image, cell_mask, nuclei_mask)
+
+        # Apply interpolations
+        cell_image, nuclei_image = self._apply_interpolation(cell_image, nuclei_image)
 
         sample = {
             'gene': gene,
@@ -76,62 +75,76 @@ class ZarrCellDataset(Dataset):
                 raise ValueError(f"Zarr file not found: {zarr_file}")
             zarr_data[(gene, barcode, stage)] = zarr.open(zarr_file, mode='r')
         return zarr_data
-
-    def _apply_transform(self, original_image, cell_mask, nuclei_mask):
-
-        if self.transform == "masks":
+      
+    def _apply_mask_normalization(self, original_image, cell_mask, nuclei_mask):
+        if self.mask == "masks":
             cell_image = original_image * cell_mask
             nuclei_image = original_image * nuclei_mask
         else:
-            raise ValueError("Only 'masks' is supported for transform")
+            raise ValueError("Only 'masks' is supported for mask")
+        cell_image = torch.from_numpy(cell_image).float()
+        nuclei_image = torch.from_numpy(nuclei_image).float()
 
-        if self.crop_size is not None:
-            center_height, center_width = original_image.shape[1] // 2, original_image.shape[2] // 2
-            slice_h = slice(center_height - self.crop_size // 2, center_height + self.crop_size // 2)
-            slice_w = slice(center_width - self.crop_size // 2, center_width + self.crop_size // 2)
-            cell_image = cell_image[:, slice_h, slice_w]
-            nuclei_image = nuclei_image[:, slice_h, slice_w]
+        if self.normalizations:
+            if isinstance(self.normalizations, list):
+                for normalization in self.normalizations:
+                    cell_image = normalization(cell_image)
+                    nuclei_image = normalization(nuclei_image)
+            else:
+                cell_image = self.normalizations(cell_image)
+                nuclei_image = self.normalizations(nuclei_image)
 
         return cell_image, nuclei_image
 
+    def _apply_interpolation(self, cell_image, nuclei_image):
+        if self.interpolations:
+            if isinstance(self.interpolations, list):
+                for interpolation in self.interpolations:
+                    cell_image, nuclei_image = interpolation(cell_image, nuclei_image)
+            else:
+                cell_image, nuclei_image = self.interpolations(cell_image, nuclei_image)
+        return cell_image, nuclei_image
+
 class ZarrCellDataset_specific(Dataset):
-    def __init__(self, parent_dir, gene_name, barcode_name, channels=[0, 1, 2, 3],
-                 cell_cycle_stages="interphase", transform="masks", crop_size=None):
+    def __init__(self, parent_dir, gene_name, barcode_name, channels=[0, 1, 2, 3], cell_cycle_stages="interphase", 
+                 mask="masks", normalizations=None, interpolations=None):
         self.parent_dir = parent_dir
         self.gene_name = gene_name
         self.barcode_name = barcode_name
         self.channels = channels
         self.cell_cycle_stages = cell_cycle_stages
-        self.transform = transform
-        self.crop_size = crop_size
+        self.mask = mask
+        self.normalizations = normalizations
+        self.interpolations = interpolations
 
         self.zarr_data = self._load_zarr_data()
         self.original_images, self.cell_masks, self.nuclei_masks = self._load_images_and_masks()
-        self.cell_images, self.nuclei_images = self._apply_transform()
 
     def __len__(self):
         return len(self.original_images)
 
     def __getitem__(self, idx):
+        original_image = self.original_images[idx]
+        cell_mask = self.cell_masks[idx]
+        nuclei_mask = self.nuclei_masks[idx]
+
+        # Apply mask and normalization
+        cell_image, nuclei_image = self._apply_mask_normalization(original_image, cell_mask, nuclei_mask)
+
+        # Apply interpolations
+        cell_image, nuclei_image = self._apply_interpolation(cell_image, nuclei_image)
+
         sample = {
             'gene': self.gene_name,
             'barcode': self.barcode_name,
             'stage': self.cell_cycle_stages,
-            'original_image': self.original_images[idx],
-            'cell_mask': self.cell_masks[idx],
-            'nuclei_mask': self.nuclei_masks[idx],
-            'cell_image': self.cell_images[idx],
-            'nuclei_image': self.nuclei_images[idx]
+            'original_image': original_image,
+            'cell_mask': cell_mask,
+            'nuclei_mask': nuclei_mask,
+            'cell_image': cell_image,
+            'nuclei_image': nuclei_image
         }
         return sample
-
-    def _read_zattrs(self, path):
-        zattrs = {}
-        zattrs_path = os.path.join(path, ".zattrs")
-        if os.path.exists(zattrs_path):
-            with open(zattrs_path, "r") as f:
-                zattrs = json.load(f)
-        return zattrs
 
     def _load_zarr_data(self):
         zarr_file_gene = os.path.join(self.parent_dir, f"{self.gene_name}.zarr")
@@ -163,19 +176,39 @@ class ZarrCellDataset_specific(Dataset):
 
         return original_images, cell_masks, nuclei_masks
 
-    def _apply_transform(self):
-        if self.transform == "masks":
-            cell_images = self.original_images * self.cell_masks
-            nuclei_images = self.original_images * self.nuclei_masks
+    def _apply_mask_normalization(self, original_image, cell_mask, nuclei_mask):
+        if self.mask == "masks":
+            cell_image = original_image * cell_mask
+            nuclei_image = original_image * nuclei_mask
         else:
-            raise ValueError("Only 'masks' is supported for transform")
+            raise ValueError("Only 'masks' is supported for mask")
+        cell_image = torch.from_numpy(cell_image).float()
+        nuclei_image = torch.from_numpy(nuclei_image).float()
 
-        if self.crop_size is not None:
-            center_height, center_width = self.original_images.shape[2] // 2, self.original_images.shape[3] // 2
-            slice_h = slice(center_height - self.crop_size // 2, center_height + self.crop_size // 2)
-            slice_w = slice(center_width - self.crop_size // 2, center_width + self.crop_size // 2)
-            cell_images = cell_images[:, :, slice_h, slice_w]
-            nuclei_images = nuclei_images[:, :, slice_h, slice_w]
+        if self.normalizations:
+            if isinstance(self.normalizations, list):
+                for normalization in self.normalizations:
+                    cell_image = normalization(cell_image)
+                    nuclei_image = normalization(nuclei_image)
+            else:
+                cell_image = self.normalizations(cell_image)
+                nuclei_image = self.normalizations(nuclei_image)
 
-        return cell_images, nuclei_images
+        return cell_image, nuclei_image
 
+    def _apply_interpolation(self, cell_image, nuclei_image):
+        if self.interpolations:
+            if isinstance(self.interpolations, list):
+                for interpolation in self.interpolations:
+                    cell_image, nuclei_image = interpolation(cell_image, nuclei_image)
+            else:
+                cell_image, nuclei_image = self.interpolations(cell_image, nuclei_image)
+        return cell_image, nuclei_image
+
+    def _read_zattrs(self, path):
+        zattrs = {}
+        zattrs_path = os.path.join(path, ".zattrs")
+        if os.path.exists(zattrs_path):
+            with open(zattrs_path, "r") as f:
+                zattrs = json.load(f)
+        return zattrs
