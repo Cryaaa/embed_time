f443fe08cd44d717db950127dac8f202ab270c42
Cryaaa <rzan.savillweb.degit config --global user.email ryan.savillweb.de>
2024-08-31 14:42:58 +0000

    added unet based decoder code still unfinished, implemented first training loop with vanilla model

--- scripts/train.py
+++ scripts/train.py
@@ -19,13 +19,14 @@ from embed_time.dataloader import LiveTLSDataset
 
 # return reconstruction error + KL divergence losses
 def loss_function(recon_x, x, mu, log_var):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    MSE = F.mse_loss(recon_x,x,reduction='mean')
     KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-    return BCE + KLD
+    return MSE + KLD
 
-def train(epoch, model, loss_fn, optimizer, train_loader):
+def train(epoch, model, loss_fn, optimizer, train_loader,checkpoint_dir):
     model.train()
     train_loss = 0
+    losses = []
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.cuda()
         optimizer.zero_grad()
@@ -37,58 +38,33 @@ def train(epoch, model, loss_fn, optimizer, train_loader):
         train_loss += loss.item()
         optimizer.step()
         
-        if batch_idx % 100 == 0:
+        if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item() / len(data)))
+        losses.append(loss.item())
+    
     print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)))
 
-def train_classifier(base_dir, loss_fn, epochs=25):
-    checkpoint_dir = Path(base_dir) / "../checkpoints"
-    checkpoint_dir.mkdir(exist_ok=True)
-    data_dir = Path(base_dir) / "../data"
-    data_dir.mkdir(exist_ok=True)
-    
-    encoder = Encoder(input_shape=...,
-                      x_dim=...,
-                      h_dim1=...,
-                      h_dim2=...,
-                      z_dim=...)
-    decoder = Decoder(z_dim=...,
-                      h_dim1=...,
-                      h_dim2=...,
-                      x_dim=...,
-                      output_shape=...)
-    model = VAE(encoder, decoder)
-    data = LiveTLSDataset(data_dir, download=True, train=True)
-    dataloader = DataLoader(data, batch_size=32, shuffle=True, pin_memory=True)
+    PATH = os.path.join(checkpoint_dir, f'chkpnt_e{epoch}.pth')
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-    losses = []
-    for epoch in range(epochs):
-        for x, y in tqdm(dataloader, desc=f"Epoch {epoch}"):
-            optimizer.zero_grad()
-            y_pred = model(x.to(device))
-            loss = loss_fn(y_pred, y.to(device))
-            loss.backward()
-            optimizer.step()
-        print(f"Epoch {epoch}: Loss = {loss.item()}")
-        losses.append(loss.item())
-        # TODO save every epoch instead of overwriting?
-        torch.save(model.state_dict(), checkpoint_dir / "model.pth")
-
-    with open(checkpoint_dir / "losses.txt", "w") as f:
-        f.write("\n".join(str(l) for l in losses))
-
+    torch.save(
+        {
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "epoch": epoch,
+        },
+        checkpoint_dir / f"checkpoint_{epoch}.pth",
+    )
+from datetime import datetime
 
 if __name__ == "__main__":
-    # checkpoint_dir = Path(base_dir) / "../checkpoints"
-    # checkpoint_dir.mkdir(exist_ok=True)
-    data_location = "/mnt/efs/dlmbl/G-et/data/live-TLS"
+    base_dir = "/mnt/efs/dlmbl/G-et/checkpoints/time-series"
+    checkpoint_dir = Path(base_dir) / f"{datetime.today().strftime('%Y-%m-%d')}_checkpoints"
+    print(checkpoint_dir)
 
+    checkpoint_dir.mkdir(exist_ok=True)
+    data_location = "/mnt/efs/dlmbl/G-et/data/live-TLS"
     folder_imgs = data_location +"/"+'Control_Dataset_4TP_Normalized'
     metadata = data_location + "/" +'Control_Dataset_4TP_Ground_Truth'
 
@@ -108,31 +84,42 @@ if __name__ == "__main__":
         metadata,
         folder_imgs,
         metadata_columns=["Run","Plate","ID"],
-        return_metadata=True,
+        return_metadata=False,
         transform = loading_transforms,
     )
-    
-    
+
+    sample, label = dataset_w_t[0]
+    in_channels, y, x = sample.shape
+    print(in_channels)
+    print((y,x))
 
     NUM_EPOCHS = 50
-    encoder = Encoder(input_shape=...,
-                      x_dim=...,
-                      h_dim1=...,
-                      h_dim2=...,
-                      z_dim=...)
-    decoder = Decoder(z_dim=...,
-                      h_dim1=...,
-                      h_dim2=...,
-                      x_dim=...,
-                      output_shape=...)
+    encoder = Encoder(input_shape=(y,x),
+                      x_dim=in_channels,
+                      h_dim1=8,
+                      h_dim2=16,
+                      z_dim=10)
+    decoder = Decoder(z_dim=10,
+                      h_dim1=16,
+                      h_dim2=8,
+                      x_dim=2,
+                      output_shape=(y,x))
     model = VAE(encoder, decoder)
-    data = LiveTLSDataset(data_dir, download=True, train=True)
-    dataloader = DataLoader(data, batch_size=32, shuffle=True, pin_memory=True)
+    dataloader = DataLoader(dataset_w_t, batch_size=4, shuffle=True, pin_memory=True)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
-    
+    print(device)
     for epoch in range(NUM_EPOCHS):
-        train(epoch)
+        train(
+            epoch,
+            model,
+            loss_function,
+            optimizer,
+            dataloader,
+            checkpoint_dir=checkpoint_dir)
         # test()
+    
+
+   
