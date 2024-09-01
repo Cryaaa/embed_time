189013b912345e64f7d991f4be19ff767cc03d25
MariaTheiss <theiss@hms.harvard.edu>
2024-09-01 00:27:39 +0000

    added label"

--- scripts/train.py
+++ scripts/train.py
@@ -9,22 +9,78 @@ from funlib.persistence import Array
 import dask #like np but on big data
 from embed_time.model import ResNet2D
 import torch
+import re
+import numpy as np
 
 
 raw = gp.ArrayKey('RAW') # this the raw image
 mask = gp.ArrayKey('MASK') # this is the mask
+label = gp.ArrayKey('LABEL') # this is the label
 
 datapath = Path("/mnt/efs/dlmbl/G-et/data/mousepanc") # path to the data
 sources = []
+
+class AddLabel(gp.BatchFilter):
+
+    def __init__(self, array_key, label):
+        self.array_key = array_key
+        self.label = label
+
+    def setup(self):
+
+        self.provides(self.array_key, gp.ArraySpec(nonspatial=True))
+
+    def prepare(self, request):
+        pass
+
+    def process(self, batch, request):
+
+        array = gp.Array(np.array(self.label), spec=gp.ArraySpec(nonspatial=True))
+        batch = gp.Batch()
+        batch[self.array_key] = array
+        return batch
+
+# def getlabel(name, labels):
+#     if '25868' in name:
+#         if bool(re.search('1.', name)):
+#             labels.append(1) #healthy
+#         elif re.search('2.', name):
+#             labels.append(2) #sick
+#         else:
+#             labels.append(3)
+#     elif '14550' in name:
+#         if re.search("A*PBS", name):
+#             labels.append(1)
+#         elif 'CaerPBS' in name:
+#             labels.append(2)
+#         elif bool(re.search("F*PBS*", name)):
+#             labels.append(3)
+#     else: 
+        
+#     return labels
+
+
+
+labels = []
+
 for test_zarr in (datapath / "zarrtransposed").iterdir():
     test_image = Array(zarr.open(datapath / "zarrtransposed"/ test_zarr.name), axis_names=["color^", "x", "y"])
     test_mask = Array(zarr.open(datapath / "masks" / test_zarr.name), axis_names=["x", "y"], voxel_size=(16, 16))
+    
+    #generate random labels 
+    gtlabel = np.random.randint(0, 3)
+    
+    #label = np.ones(test_image.shape) * gtlabel
 
     image_source = gp.ArraySource(raw, test_image, interpolatable=True) # put image into gunpowder pipeline
     mask_source = gp.ArraySource(mask, test_mask, interpolatable=False) # put mask into gunpowder pipeline
+    #label_source = gp.ArraySource(label, np.array(gtlabel), interpolatable=False)
+
+
 
     source = (image_source, mask_source) + gp.MergeProvider() # put both into pipeline
     source += gp.RandomLocation(mask = mask, min_masked = 0.9) # random location in image. at least 90% of the patch is foreground
+    source += AddLabel(label, gtlabel) # "for this testzar my label is x"
     sources.append(source)
     #break
 
@@ -41,6 +97,7 @@ request = gp.BatchRequest()
 size = 320
 request.add(raw, (size, size))
 request.add(mask, (size, size))
+request[label] = gp.ArraySpec(nonspatial=True)
 
 output_classes = 256 # dimensions of the latent space
 input_channels = 3
