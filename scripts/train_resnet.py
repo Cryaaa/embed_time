c61d5bf68ba146b9075cc4b655c3a96de1e1e032
MariaTheiss <theiss@hms.harvard.edu>
2024-09-01 23:36:37 +0000

    trying to train resnet classifier

---
+++ scripts/train_resnet.py
@@ -0,0 +1,184 @@
+import zarr
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from pathlib import Path
+import gunpowder as gp
+from funlib.persistence import Array
+import dask #like np but on big data
+#from embed_time.model import ResNet2D
+from embed_time.resnet import ResNet18
+import torch
+import re
+import numpy as np
+from tqdm import tqdm
+
+
+raw = gp.ArrayKey('RAW') # this the raw image
+mask = gp.ArrayKey('MASK') # this is the mask
+label = gp.ArrayKey('LABEL') # this is the label
+
+datapath = Path("/mnt/efs/dlmbl/G-et/data/mousepanc") # path to the data
+sources = []
+
+class AddLabel(gp.BatchFilter):
+
+    def __init__(self, array_key, label):
+        self.array_key = array_key
+        self.label = label
+
+    def setup(self):
+
+        self.provides(self.array_key, gp.ArraySpec(nonspatial=True))
+
+    def prepare(self, request):
+        pass
+
+    def process(self, batch, request):
+
+        array = gp.Array(np.array(self.label), spec=gp.ArraySpec(nonspatial=True))
+        batch = gp.Batch()
+        batch[self.array_key] = array
+        return batch
+
+
+
+labels = []
+
+def getlabel(test_zarr):
+    if '25868' in test_zarr.name:
+        if '25868$1.' in test_zarr.name:
+            return 0
+        elif '25868$2.' in test_zarr.name:
+            return 1
+        else: 
+            return 2
+
+
+
+for test_zarr in (datapath / "zarrtransposed").iterdir():
+
+    if not "25868" in test_zarr.name: # subset to only 25868 for now 
+        print("skipping", test_zarr.name)
+        continue
+
+    test_image = Array(zarr.open(datapath / "zarrtransposed"/ test_zarr.name), axis_names=["color^", "x", "y"])
+    test_mask = Array(zarr.open(datapath / "masks" / test_zarr.name), axis_names=["x", "y"], voxel_size=(16, 16))
+    
+    #print(test_zarr.name)
+    gtlabel = getlabel(test_zarr) #np.random.randint(0, 3) #generate random labels 
+    #print(gtlabel)
+    
+    image_source = gp.ArraySource(raw, test_image, interpolatable=True) # put image into gunpowder pipeline
+    mask_source = gp.ArraySource(mask, test_mask, interpolatable=False) # put mask into gunpowder pipeline
+
+    source = (image_source, mask_source) + gp.MergeProvider() # put both into pipeline
+    source += gp.RandomLocation(mask = mask, min_masked = 0.9) # random location in image. at least 90% of the patch is foreground
+    source += AddLabel(label, gtlabel) # "for this testzar my label is x"
+    sources.append(source)
+    break
+
+source = tuple(sources) + gp.RandomProvider()
+
+# Augment image
+source += gp.Normalize(raw) # normalize image: devides by max (255)
+# TODO: normalize using mean and std of data 
+
+source += gp.DeformAugment(control_point_spacing=gp.Coordinate(100, 100), jitter_sigma=gp.Coordinate(10.0, 10.0), rotate=True, spatial_dims = 2) # augment image
+#TODO: Add more augmentations
+batch_size = 16
+source += gp.Stack(batch_size) # stack the batch
+
+# write request
+request = gp.BatchRequest()
+size = 64
+request.add(raw, (size, size))
+request.add(mask, (size, size))
+request[label] = gp.ArraySpec(nonspatial=True)
+
+z_dim = 128 # dimensions of the latent space
+input_channels = 3
+n_iter = 100
+
+resnet = ResNet18(nc = input_channels)
+loss_function: torch.nn.Module = torch.nn.MSELoss()
+
+kl_divergence = torch.nn.KLDivLoss()
+optimizer = torch.optim.Adam(vae.parameters(), lr=0.001)
+
+device = torch.device("cuda")
+
+vae.train()
+vae = vae.to(device)
+
+losses = []
+
+
+with gp.build(source):
+    for n in tqdm(range(n_iter)): # number of iterations
+        batch = source.request_batch(request)
+        
+        x_in = batch.arrays[raw].data
+
+        x_in_t = torch.tensor(x_in, dtype=torch.float32)
+
+        x_in_t = x_in_t.to(device)
+        optimizer.zero_grad()
+
+
+        x, z = resnet(x_in_t) # data goes to the foreward function
+        rec_loss = loss_function(x, x_in_t)
+
+        kl_loss = kld_loss(mean, logvar)
+
+
+        loss = beta * kl_loss + rec_loss
+        losses.append(loss.item())
+        reclosses.append(rec_loss.item())
+        kllosses.append(kl_loss.item())
+        loss.backward()
+        optimizer.step()
+
+        # add a loss-function
+
+
+
+def plotoutput(x_in, x_out):
+    np.random.seed(0)
+    shuffled = np.arange(len(x_in))
+
+    np.random.shuffle(shuffled)
+    fig, ax = plt.subplots(2,4)
+
+    for i in range(4):
+        ax[0, i].imshow(x_in_t[shuffled[i]].cpu().detach().numpy().transpose(1,2,0))
+        ax[1, i].imshow(x_out[shuffled[i]].cpu().detach().numpy().transpose(1,2,0))
+
+    plt.show()
+
+
+plotoutput(x_in_t, x)
+
+
+fig, ax = plt.subplots(1,2)
+ax[0].plot(reclosses)
+ax[0].set_title("Reconstruction loss")
+ax[1].plot(kllosses)
+ax[1].set_title("KL loss")
+
+# treat this as dataloader. Same pipeline for sick and treated data 
+# put x through the model 
+# 
+
+# output space: dimensions of the latent space 
+
+
+# How to tmux
+
+# tmux new -s train # where train is the name of the session
+# tmux ls # list all sessions
+# tmux a -t train # attach to session train
+# conda activate embed_time
+# python scripts/train.py
+# control + b, d # detach from session
---
+++ src/embed_time/resnet.py
@@ -0,0 +1,101 @@
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+
+class ResizeConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, scale_factor, mode='nearest'):
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.mode = mode
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=kernel_size//2)
+    
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+        x = self.conv(x)
+        return x
+
+class BasicBlockEnc(nn.Module):
+
+    def __init__(self, in_planes, stride=1):
+        super().__init__()
+
+        planes = in_planes*stride
+
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        if stride == 1:
+            self.shortcut = nn.Identity()
+        else:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out = out + self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+
+class ResNet18Enc(nn.Module):
+
+    def __init__(self, num_Blocks=[2,2,2,2], z_dim=10, nc=3):
+        super().__init__()
+        self.in_planes = 64
+        self.z_dim = z_dim
+        self.conv1 = nn.Conv2d(nc, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(BasicBlockEnc, 64, num_Blocks[0], stride=1)
+        self.layer2 = self._make_layer(BasicBlockEnc, 128, num_Blocks[1], stride=2)
+        self.layer3 = self._make_layer(BasicBlockEnc, 256, num_Blocks[2], stride=2)
+        self.layer4 = self._make_layer(BasicBlockEnc, 512, num_Blocks[3], stride=2)
+        self.linear = nn.Conv2d(512, 2 * z_dim, kernel_size=1)
+        #self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+    def _make_layer(self, BasicBlockEnc, planes, num_Blocks, stride):
+        strides = [stride] + [1]*(num_Blocks-1)
+        layers = []
+        for stride in strides:
+            layers += [BasicBlockEnc(self.in_planes, stride)]
+            self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.linear(x)
+        print(x.shape)
+        mu, logvar = torch.chunk(x, 2, dim=1)
+        return mu, logvar
+
+    
+class ResNet18(nn.Module):
+    
+    def __init__(self, nc, z_dim):
+        super().__init__()
+        self.encoder = ResNet18Enc(nc=nc, z_dim=z_dim)
+
+        # TODO: global average pooling 
+        self.dense = nn.Sequential(
+            nn.Linear(in_features=32 * 7 * 7, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=10),
+        )
+
+
+
+    def forward(self, x):
+        mean, logvar = self.encoder(x)
+
+        x = self.decoder(z)
+        return x, 
+    
+
