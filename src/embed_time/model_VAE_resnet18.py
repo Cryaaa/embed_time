daa344cb6783cd20ccbc328ffbdfeb8228bffd8d
Ben Salmon <brs209@student.bham.ac.uk>
2024-09-01 02:00:45 +0000

    no bugs, yet to train

--- src/embed_time/model_VAE_resnet18.py
+++ src/embed_time/model_VAE_resnet18.py
@@ -7,10 +7,10 @@ class ResizeConv2d(nn.Module):
         super().__init__()
         self.scale_factor = scale_factor
         self.mode = mode
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=1)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=kernel_size//2)
     
     def forward(self, x):
-        F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
         x = self.conv(x)
         return x
 
@@ -27,7 +27,7 @@ class BasicBlockEnc(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
 
         if stride == 1:
-            self.shortcut = nn.Sequential()
+            self.shortcut = nn.Identity()
         else:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
@@ -63,7 +63,7 @@ class BasicBlockDec(nn.Module):
                 nn.BatchNorm2d(planes)
             )
     
-    def foward(self, x):
+    def forward(self, x):
         out = torch.relu(self.bn2(self.conv2(x)))
         out = self.bn1(self.conv1(out))
         out += self.shortcut(x)
@@ -83,7 +83,7 @@ class ResNet18Enc(nn.Module):
         self.layer2 = self._make_layer(BasicBlockEnc, 128, num_Blocks[1], stride=2)
         self.layer3 = self._make_layer(BasicBlockEnc, 256, num_Blocks[2], stride=2)
         self.layer4 = self._make_layer(BasicBlockEnc, 512, num_Blocks[3], stride=2)
-        self.linear = nn.Linear(512, 2 * z_dim)
+        self.linear = nn.Conv2d(512, 2 * z_dim, kernel_size=1)
 
     def _make_layer(self, BasicBlockEnc, planes, num_Blocks, stride):
         strides = [stride] + [1]*(num_Blocks-1)
@@ -99,11 +99,8 @@ class ResNet18Enc(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = F.adaptive_avg_pool2d(x, 1)
-        x = x.view(x.size(0), -1)
         x = self.linear(x)
-        mu = x[:, :self.z_dim]
-        logvar = x[:, self.z_dim:]
+        mu, logvar = torch.chunk(x, 2, dim=1)
         return mu, logvar
 
 class ResNet18Dec(nn.Module):
@@ -111,8 +108,9 @@ class ResNet18Dec(nn.Module):
     def __init__(self, num_Blocks=[2,2,2,2], z_dim=10, nc=3):
         super().__init__()
         self.in_planes = 512
+        self.nc = nc
 
-        self.linear = nn.Linear(z_dim, 512)
+        self.linear = nn.Conv2d(z_dim, 512, kernel_size=1)
 
         self.layer4 = self._make_layer(BasicBlockDec, 256, num_Blocks[3], stride=2)
         self.layer3 = self._make_layer(BasicBlockDec, 128, num_Blocks[2], stride=2)
@@ -130,25 +128,22 @@ class ResNet18Dec(nn.Module):
 
     def forward(self, z):
         x = self.linear(z)
-        x = x.view(z.size(0), 512, 1, 1)
-        x = F.interpolate(x, scale_factor=4)
         x = self.layer4(x)
         x = self.layer3(x)
         x = self.layer2(x)
         x = self.layer1(x)
         x = torch.sigmoid(self.conv1(x))
-        x = x.view(x.size(0), 3, 64, 64)
         return x
     
     
 class VAEResNet18(nn.Module):
     
-    def __init__(self, z_dim):
+    def __init__(self, nc, z_dim):
         super().__init__()
-        self.encoder = ResNet18Enc(z_dim=z_dim)
-        self.decoder = ResNet18Dec(z_dim=z_dim)
+        self.encoder = ResNet18Enc(nc=nc, z_dim=z_dim)
+        self.decoder = ResNet18Dec(nc=nc, z_dim=z_dim)
 
-    def foward(self, x):
+    def forward(self, x):
         mean, logvar = self.encoder(x)
         z = self.reparameterize(mean, logvar)
         x = self.decoder(z)
@@ -157,5 +152,5 @@ class VAEResNet18(nn.Module):
     @staticmethod
     def reparameterize(mean, logvar):
         std = torch.exp(logvar / 2) # in log-space, squareroot is divide by two
-        epsilon = torch.rand_like(std)
+        epsilon = torch.randn_like(std)
         return epsilon * std + mean
