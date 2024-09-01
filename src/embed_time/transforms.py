added normalization notebook, investigation notebook, new train and some minor bug fixes

src/embed_time/transforms.py
@@ -84,6 +84,25 @@ class SelectRandomTimepoint(object):
             random_tp if i == self.td else slice(0,shape[i]) for i in range(len(shape))
         ]
         return sample[slice_objects]
+    
+class SelectSpecificTimepoint(object):
+    """select a random timepoint form the time series
+
+    time_dimension: int
+        dimension index of time 
+    """
+
+    def __init__(self, time_dimension, timepoint):
+        self.td = time_dimension
+        self.timepoint = timepoint
+
+    def __call__(self, sample):
+        shape = sample.shape
+        
+        slice_objects = [
+            self.timepoint if i == self.td else slice(0,shape[i]) for i in range(len(shape))
+        ]
+        return sample[slice_objects]
 
 class SelectRandomTPNumpy(object):
     """select a random timepoint form the time series
@@ -94,13 +113,28 @@ class SelectRandomTPNumpy(object):
 
     def __init__(self, time_dimension):
         self.td = time_dimension
-
+        
     def __call__(self, sample):
         shape = sample.shape
         random_tp = np.random.randint(0,shape[self.td])
         
         out = np.take(sample,[random_tp],axis=self.td).squeeze(self.td)
         # print(out.shape)
+        return out
+    
+class SelectSpecificTPNumpy(object):
+    """select a random timepoint form the time series
+
+    time_dimension: int
+        dimension index of time 
+    """
+
+    def __init__(self, time_dimension, timepoint):
+        self.td = time_dimension
+        self.timepoint = timepoint
+
+    def __call__(self, sample):
+        out = np.take(sample,[self.timepoint],axis=self.td).squeeze(self.td)
         return out
 
 class CustomToTensor(object):